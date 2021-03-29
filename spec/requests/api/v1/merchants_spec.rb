a0900041bbdbf360b0e3dd1d53a5df2b7d0ed6ea
Travis <mckinstrytg1@gmail.com>
2021-03-29 13:12:41 -0600

    Passes all Merchants request call spec

--- spec/requests/api/v1/merchants_spec.rb
+++ spec/requests/api/v1/merchants_spec.rb
@@ -1,17 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchants' do
-  def conn(uri)
-    url = ENV['RAILS_ENGINE_DOMAIN'] + uri
-    Faraday.new(url)
-  end
   describe 'REST endpoints' do
-    it 'can get all Merchants' do
+    before(:each) do
       create_list(:merchant, 5)
-
-      response = conn('/api/v1/merchants').get
+      get '/api/v1/merchants'
+    end
+    it 'can make successful API call for all Merchants' do
       expect(response).to be_successful
-      require "pry"; binding.pry
+      expect(response.body).to be_a(String)
+    end
+    it '/merchants sends objects with ID, name, created & updated_at attributes' do
+      merchants = JSON.parse(response.body, symbolize_names: true)
+      expect(merchants).to be_a(Hash)
+      expect(merchants.keys).to eq(%i[data])
+      merchants.each do |key, value|
+        expect(value).to be_an(Array)
+        value.each do |merchant|
+          expect(merchant.keys).to eq(%i[id type attributes])
+          merchant.each do |k, v|
+            if k == :id || k == :type
+              expect(v).to be_a(String)
+            else
+              expect(v).to be_a(Hash)
+              expect(v.keys).to eq(%i[id name created_at updated_at])
+              v.each do |key1, value1|
+                if key1 == :id
+                  expect(value1).to be_an(Integer)
+                else
+                  expect(value1).to be_a(String)
+                end
+              end
+            end
+          end
+        end
+      end
     end
   end
 end
